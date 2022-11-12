cf6e427f8345b4e65dd334ea0791193708ec04fd
d-smith <doug.smith.mail@gmail.com>
2022-11-12 08:22:16 -0800

    mnemonic to entropy

--- seed/seed.go
+++ seed/seed.go
@@ -1,9 +1,17 @@
 package seed
 
+//TODO - refactor into functions, add error handling, etcc
+// initial implementation is more 'script' than package
+
+// Some code taken verbatim from https://github.com/tyler-smith/go-bip39/blob/master/bip39.go and
+// the go sample in https://learnmeabitcoin.com/technical/mnemonic
+// Refer to those sources for more details
+
 import (
 	"crypto/rand" 
 	"crypto/sha256"
     "encoding/hex"
+    "encoding/binary"
     "fmt"
 	"math/big"
 	"io/ioutil"
@@ -13,6 +21,32 @@ import (
 var (
     wordlist []string
     wordMap = map[string]int{}
+)
+
+var (
+    last11BitsMask = big.NewInt(2047) // (2047 = 0b11111111111)
+    shift11BitsMask = big.NewInt(2048) // // dividing by 2048 is the same as bit shifting 11 bits
+    bigOne          = big.NewInt(1)
+	bigTwo          = big.NewInt(2)
+
+    // wordLengthChecksumMasksMapping is used to isolate the checksum bits from
+	//the entropy+checksum byte array.
+	wordLengthChecksumMasksMapping = map[int]*big.Int{
+		12: big.NewInt(15),
+		15: big.NewInt(31),
+		18: big.NewInt(63),
+		21: big.NewInt(127),
+		24: big.NewInt(255),
+	}
+
+	// wordLengthChecksumShiftMapping is used to lookup the number of operand
+	// for shifting bits to handle checksums.
+	wordLengthChecksumShiftMapping = map[int]*big.Int{
+		12: big.NewInt(16),
+		15: big.NewInt(8),
+		18: big.NewInt(4),
+		21: big.NewInt(2),
+	}
 )
 
 
@@ -126,7 +160,7 @@ func GetMnemonicPhrase() string {
         // 11111111111|11111111111|11111111111|11111111111
         //
         word := big.NewInt(0)                  // hold result of 11 bit mask
-        word.And(dataBigInt, big.NewInt(2047)) // bit mask last 11 bits (2047 = 0b11111111111)
+        word.And(dataBigInt, last11BitsMask) // bit mask last 11 bits (2047 = 0b11111111111)
 
         // Add corresponding word to array
         //
@@ -141,7 +175,7 @@ func GetMnemonicPhrase() string {
         //                                    100000000000 = big.NewInt(2048)
         // 11111111111|11111111111|11111111111|
         //
-        dataBigInt.Div(dataBigInt, big.NewInt(2048)) // dividing by 2048 is the same as bit shifting 11 bits
+        dataBigInt.Div(dataBigInt, shift11BitsMask) // dividing by 2048 is the same as bit shifting 11 bits
 
     }
 
@@ -153,9 +187,71 @@ func GetMnemonicPhrase() string {
 }
 
 func EntropyFromMnemonic(phrase string) []byte {
+    // Split the phrase into words
     words := strings.Fields(phrase)
+
+    // Decode the words into a big int
+
+    var (
+		wordBytes [2]byte
+		b         = big.NewInt(0)
+	)
+
     for _,v := range words {
-        fmt.Println(v, " ==> ", wordMap[v])
+        index, _ := wordMap[v]
+        
+        binary.BigEndian.PutUint16(wordBytes[:], uint16(index))
+		b.Mul(b, shift11BitsMask)
+		b.Or(b, big.NewInt(0).SetBytes(wordBytes[:]))
     }
-    return make([]byte, 32);
+
+    // Build and add the checksum to the big.Int.
+	checksum := big.NewInt(0)
+	checksumMask := wordLengthChecksumMasksMapping[len(words)]
+	checksum = checksum.And(b, checksumMask)
+
+    b.Div(b, big.NewInt(0).Add(checksumMask, bigOne))
+    
+    // The entropy is the underlying bytes of the big.Int. Any upper bytes of
+	// all 0's are not returned so we pad the beginning of the slice with empty
+	// bytes if necessary.
+	entropy := b.Bytes()
+	entropy = padByteSlice(entropy, len(words)/3*4)
+
+	// Generate the checksum and compare with the one we got from the mneomnic.
+	entropyChecksumBytes := computeChecksum(entropy)
+	entropyChecksum := big.NewInt(int64(entropyChecksumBytes[0]))
+
+	if l := len(words); l != 24 {
+		checksumShift := wordLengthChecksumShiftMapping[l]
+		entropyChecksum.Div(entropyChecksum, checksumShift)
+	}
+
+	if checksum.Cmp(entropyChecksum) != 0 {
+        fmt.Println("checksum does not match")
+		return nil
+	}
+
+	return entropy
+}
+
+func computeChecksum(data []byte) []byte {
+	hasher := sha256.New()
+	_, _ = hasher.Write(data) // This error is guaranteed to be nil
+
+	return hasher.Sum(nil)
+}
+
+// padByteSlice returns a byte slice of the given size with contents of the
+// given slice left padded and any empty spaces filled with 0's.
+func padByteSlice(slice []byte, length int) []byte {
+	offset := length - len(slice)
+	if offset <= 0 {
+		return slice
+	}
+
+	newSlice := make([]byte, length)
+	copy(newSlice[offset:], slice)
+
+	return newSlice
 }
--- seed/seed_test.go
+++ seed/seed_test.go
@@ -3,10 +3,13 @@ package seed
 import (
 	"testing"
 	"fmt"
+	"math/big"
 )
 
 func TestSeedGenerated(t *testing.T) {
 	seed := GetMnemonicPhrase()
 	fmt.Println(seed)
-	EntropyFromMnemonic(seed)
+	entropy := EntropyFromMnemonic(seed)
+	dataBigInt := new(big.Int).SetBytes(entropy)
+	fmt.Println("Entropy:         ", dataBigInt)
 }
