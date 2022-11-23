86f820156d736a9261ca99b18b5a4e48bc94f4ce
d-smith <doug.smith.mail@gmail.com>
2022-11-23 12:34:56 -0800

    added balance for address sample

---
+++ samples/04-balance-for-address/bal4addr.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    "log"
+
+    "github.com/ethereum/go-ethereum/common"
+    "github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+
+ 	client, err := ethclient.Dial("http://172.17.144.1:7545")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+	//HD wallet account 0 address
+	account := common.HexToAddress("0xC49926C4124cEe1cbA0Ea94Ea31a6c12318df947")
+	balance, err := client.BalanceAt(context.Background(), account, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(balance)
+}
